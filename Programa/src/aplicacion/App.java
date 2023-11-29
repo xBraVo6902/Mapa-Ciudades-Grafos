3ca8e1858934db5e62949abb1d214acaedf41449
xStarH4ndx <146247223+xStarH4ndx@users.noreply.github.com>
2023-11-29 20:56:29 -0300

    asasas

--- Programa/src/aplicacion/App.java
+++ Programa/src/aplicacion/App.java
@@ -4,6 +4,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.Line2D;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
@@ -16,7 +17,6 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
-
 public class App {
 
     private static volatile boolean cancelado = false;
@@ -156,6 +156,153 @@ public class App {
                 }
             }
             //Termina el SwingWorker
+        };
+
+        worker.addPropertyChangeListener(evt -> {
+            if ("progress".equals(evt.getPropertyName())) {
+                SwingUtilities.invokeLater(() -> progressBar.setValue((Integer) evt.getNewValue()));
+            } else if (StateValue.DONE.equals(evt.getNewValue())) {
+                if (!cancelado) {
+                    progressDialog.dispose();
+                }
+            }
+        });
+
+        worker.execute();
+        progressDialog.setVisible(true);
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        progressDialog.dispose();
+    }
+    
+    public static void cargarOnline(String nodesXML, String edgesXML) {
+        cancelado = false;
+
+        progressDialog = new JDialog((Frame) null, "Cargando Archivo", true);
+        progressDialog.setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+        progressDialog.setLayout(new BorderLayout());
+
+        JProgressBar progressBar = new JProgressBar(0, 100);
+        progressBar.setStringPainted(true);
+
+        JButton cancelButton = new JButton("Cancelar");
+        cancelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                cancelado = true;
+                progressDialog.dispose();
+            }
+        });
+
+        JPanel panel = new JPanel(new BorderLayout());
+        panel.add(progressBar, BorderLayout.CENTER);
+        panel.add(cancelButton, BorderLayout.SOUTH);
+
+        progressDialog.add(panel, BorderLayout.CENTER);
+        progressDialog.setSize(300, 100);
+        progressDialog.setLocationRelativeTo(null);
+
+        SwingWorker<Void, Integer> worker = new SwingWorker<Void, Integer>() {
+            @Override
+            protected Void doInBackground() {
+                ArrayList<MyPoint> coordenadas = new ArrayList<>();
+                ArrayList<String> nombreCalles = new ArrayList<>();
+                ArrayList<Line2D.Double> conexiones = new ArrayList<>();
+
+                double maxX = Double.NEGATIVE_INFINITY;
+                double maxY = Double.NEGATIVE_INFINITY;
+                double minX = Double.POSITIVE_INFINITY;
+                double minY = Double.POSITIVE_INFINITY;
+
+                HashMap<Double, MyPoint> puntosPorId = new HashMap<>();
+
+                try {
+                    // Load nodes from XML string
+                    try (InputStreamReader isr = new InputStreamReader(new ByteArrayInputStream(nodesXML.getBytes()))) {
+                        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                        DocumentBuilder builder = factory.newDocumentBuilder();
+                        Document document = builder.parse(new org.xml.sax.InputSource(isr));
+                        NodeList rowElements = document.getElementsByTagName("row");
+
+                        int totalRows = rowElements.getLength();
+                        for (int i = 0; i < totalRows; i++) {
+                            if (cancelado) {
+                                break;
+                            }
+
+                            Element rowElement = (Element) rowElements.item(i);
+                            double id = Double.parseDouble(rowElement.getElementsByTagName("osmid").item(0).getTextContent());
+                            double x = Double.parseDouble(rowElement.getElementsByTagName("x").item(0).getTextContent());
+                            double y = Double.parseDouble(rowElement.getElementsByTagName("y").item(0).getTextContent());
+
+                            MyPoint punto = new MyPoint(id, x, y);
+                            coordenadas.add(punto);
+                            puntosPorId.put(id, punto);
+
+                            maxX = Math.max(maxX, x);
+                            maxY = Math.max(maxY, y);
+                            minX = Math.min(minX, x);
+                            minY = Math.min(minY, y);
+
+                            int progress = (int) ((double) i / totalRows * 50);
+                            setProgress(progress);
+                        }
+                    }
+
+                    // Load edges from XML string
+                    try (InputStreamReader isr = new InputStreamReader(new ByteArrayInputStream(edgesXML.getBytes()))) {
+                        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+                        DocumentBuilder builder = factory.newDocumentBuilder();
+                        Document document = builder.parse(new org.xml.sax.InputSource(isr));
+                        NodeList rowElements = document.getElementsByTagName("edge");
+
+                        int totalEdges = rowElements.getLength();
+                        for (int i = 0; i < totalEdges; i++) {
+                            if (cancelado) {
+                                break;
+                            }
+
+                            Element rowElement = (Element) rowElements.item(i);
+                            double id_1 = Double.parseDouble(rowElement.getElementsByTagName("u").item(0).getTextContent());
+                            double id_2 = Double.parseDouble(rowElement.getElementsByTagName("v").item(0).getTextContent());
+                            String nombreCamino = rowElement.getElementsByTagName("name").item(0).getTextContent();
+
+                            MyPoint punto1 = puntosPorId.get(id_1);
+                            MyPoint punto2 = puntosPorId.get(id_2);
+
+                            if (punto1 != null && punto2 != null) {
+                                Line2D.Double conexion = new Line2D.Double(punto1.getX(), punto1.getY(), punto2.getX(), punto2.getY());
+                                conexiones.add(conexion);
+                                nombreCalles.add(nombreCamino);
+                            }
+
+                            int progress = 51 + (int) ((double) i / totalEdges * 50);
+                            setProgress(progress);
+                        }
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                if (!cancelado) {
+                    DibujarGrafo dibujarGrafo = new DibujarGrafo(conexiones, coordenadas, minX, minY, maxX, maxY, nombreCalles);
+                    dibujarGrafo.dibujar();
+                }
+                return null;
+            }
+
+            @Override
+            protected void done() {
+                if (!cancelado) {
+                    progressDialog.dispose();
+                }
+            }
         };
 
         worker.addPropertyChangeListener(evt -> {
--- Programa/src/paneles/panelOnline.java
+++ Programa/src/paneles/panelOnline.java
@@ -1,38 +1,90 @@
 package paneles;
 
+import aplicacion.CiudadesProvider;
+import aplicacion.App;
 
 import javax.swing.*;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class panelOnline extends JPanel {
     private static final long serialVersionUID = 1L;
 
-    public panelOnline() {
-        JButton buttonChooseFile1 = new JButton("Archivo Nodos");
-        JButton buttonChooseFile2 = new JButton("Archivo Edges");
+    private JComboBox<String> comboBox;
 
+    public panelOnline() {
         // Configurar el layout del JPanel
         SpringLayout springLayout = new SpringLayout();
-        springLayout.putConstraint(SpringLayout.NORTH, buttonChooseFile2, 7, SpringLayout.SOUTH, buttonChooseFile1);
-        springLayout.putConstraint(SpringLayout.NORTH, buttonChooseFile1, 66, SpringLayout.NORTH, this);
-        springLayout.putConstraint(SpringLayout.WEST, buttonChooseFile2, 248, SpringLayout.WEST, this);
-        springLayout.putConstraint(SpringLayout.EAST, buttonChooseFile2, -67, SpringLayout.EAST, this);
-        springLayout.putConstraint(SpringLayout.EAST, buttonChooseFile1, -1, SpringLayout.EAST, buttonChooseFile2);
-        springLayout.putConstraint(SpringLayout.WEST, buttonChooseFile1, 0, SpringLayout.WEST, buttonChooseFile2);
         setLayout(springLayout);
-        add(buttonChooseFile1);
-        add(buttonChooseFile2);
-        
+
         JLabel lblOnline = new JLabel("ONLINE");
         springLayout.putConstraint(SpringLayout.NORTH, lblOnline, 10, SpringLayout.NORTH, this);
         springLayout.putConstraint(SpringLayout.WEST, lblOnline, 179, SpringLayout.WEST, this);
         lblOnline.setFont(new Font("Tahoma", Font.BOLD, 25));
         add(lblOnline);
 
+        // Obtener la lista de ciudades desde CiudadesProvider.list()
+        List<String> ciudadesList;
+        try {
+            ciudadesList = CiudadesProvider.instance().list();
+        } catch (IOException e) {
+            e.printStackTrace();
+            ciudadesList = new ArrayList<>(); // Manejar la excepción según tus necesidades
+        }
 
-     
+        // Convertir la lista a un array para usar en el JComboBox
+        String[] opcionesCiudades = ciudadesList.toArray(new String[0]);
+
+        comboBox = new JComboBox<>(opcionesCiudades);
+        springLayout.putConstraint(SpringLayout.NORTH, comboBox, 45, SpringLayout.SOUTH, lblOnline);
+        add(comboBox);
+
+        JLabel lblNewLabel = new JLabel("Ciudad:");
+        springLayout.putConstraint(SpringLayout.WEST, comboBox, 21, SpringLayout.EAST, lblNewLabel);
+        springLayout.putConstraint(SpringLayout.EAST, comboBox, 218, SpringLayout.EAST, lblNewLabel);
+        springLayout.putConstraint(SpringLayout.NORTH, lblNewLabel, 83, SpringLayout.NORTH, this);
+        springLayout.putConstraint(SpringLayout.WEST, lblNewLabel, 44, SpringLayout.WEST, this);
+        lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 18));
+        add(lblNewLabel);
+
+        JButton btnMostrarMapa = new JButton("Mostrar Mapa");
+        springLayout.putConstraint(SpringLayout.NORTH, btnMostrarMapa, 20, SpringLayout.SOUTH, comboBox);
+        springLayout.putConstraint(SpringLayout.WEST, btnMostrarMapa, 0, SpringLayout.WEST, comboBox);
+        springLayout.putConstraint(SpringLayout.EAST, btnMostrarMapa, 0, SpringLayout.EAST, comboBox);
+        add(btnMostrarMapa);
+
+        // Agregar ActionListener al botón "Mostrar Mapa"
+        btnMostrarMapa.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String ciudadSeleccionada = (String) comboBox.getSelectedItem();
+                if (!ciudadSeleccionada.equals("---SELECCIONE UNA CIUDAD---")) {
+                    try {
+                        // Obtener instancia de CiudadesProvider
+                        CiudadesProvider ciudadesProvider = CiudadesProvider.instance();
+
+                        // Obtener ubicaciones de los archivos XML para la ciudad seleccionada
+                        CiudadesProvider.Ciudad ciudad = ciudadesProvider.ciudad(ciudadSeleccionada);
+                        String xmlNodes = ciudad.getXmlNodes();
+                        String xmlEdges = ciudad.getXmlEdges();
+
+                        // Llamar a la función cargarOnline de la clase App
+                        App.cargarOnline(xmlNodes, xmlEdges);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                        // Manejar la excepción según tus necesidades.
+                    }
+                } else {
+                    // Manejar el caso en que no se ha seleccionado ninguna ciudad
+                    System.out.println("Por favor, seleccione una ciudad antes de mostrar el mapa.");
+                }
+            }
+        });
     }
-
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
